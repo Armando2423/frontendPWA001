16c06a23b2ab246355005411f2054835004af355
Armando2423 <sergio.reyes.21m@utzmg.edu.mx>
2025-03-28 14:30:52 -0600

    again X2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port:  3005, // Usa el puerto de Render o el 3005 localmente
+    host: true, // Permite acceder desde cualquier IP
+   /*  headers: {
+      "Content-Security-Policy": "default-src 'self'; img-src * data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+    } */
+  },
+  preview: {
+    port:  3000,
+    host: true
+  }
 })
+  
